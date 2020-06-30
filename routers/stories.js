ec20d09732e8db9f0c2e228ebae2ac67c6371779
hanhngooo <hanhngo1795@gmail.com>
2020-06-30 12:22:27 +0200

    Add stories/:storyLineId router with GET

--- routers/stories.js
+++ routers/stories.js
@@ -13,5 +13,17 @@ router.get("/", async (request, response, next) => {
     next(error);
   }
 });
-
+router.get("/:storyLineId", async (request, response, next) => {
+  try {
+    const stories = await Story.findAll({
+      where: { storyLineId: request.params.storyLineId },
+    });
+    if (!stories) {
+      return response.status(403).send({ message: "no story lines found!" });
+    }
+    return response.status(200).send(stories);
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
