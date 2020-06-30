98fbe4e94b5fd090894b844fae65860749ed4c16
nazneen1022 <nazneen1022@gmail.com>
2020-06-30 11:52:18 +0200

    added POST method to the router

--- routers/storyline.js
+++ routers/storyline.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const Storyline = require("../models").storyLine;
-//console.log("naz:", require("../models"));
+const auth = require("../auth/middleware");
 
 const router = new Router();
 
@@ -11,6 +11,27 @@ router.get("/", async (request, response, next) => {
       return response.status(403).send({ message: "no story lines found!" });
     }
     return response.status(200).send(storylines);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", auth, async (request, response, next) => {
+  console.log("request.body:", request.body);
+  const { content } = request.body;
+
+  try {
+    if (!content) {
+      return response
+        .status(403)
+        .send({ message: "content of the new storyline cannot be blank" });
+    }
+    const storyline = await Storyline.create({ content });
+
+    if (!storyline) {
+      return response.status(403).send({ message: "no story lines created!" });
+    }
+    return response.status(200).send(storyline);
   } catch (error) {
     next(error);
   }
