d806945d44d655b8b641b5cee6029724c6a3ba43
hanhngooo <hanhngo1795@gmail.com>
2020-07-02 15:50:09 +0200

    Add dummy comments, Add a route to get comments of a story

--- routers/stories.js
+++ routers/stories.js
@@ -5,10 +5,9 @@ const auth = require("../auth/middleware");
 const Story = require("../models").story;
 const User = require("../models").user;
 const Rating = require("../models").rating;
+const Comment = require("../models").comment;
+
 const router = new Router();
-
-
-
 
 router.post("/:storyLineId", auth, async (request, response, next) => {
   console.log("request.body:", request.body, request.params);
@@ -49,7 +48,6 @@ router.post("/:storyLineId", auth, async (request, response, next) => {
   }
 });
 
-
 // get stories with same story line
 
 router.get("/:storyLineId", async (request, response, next) => {
@@ -66,7 +64,6 @@ router.get("/:storyLineId", async (request, response, next) => {
   }
 });
 
-
 // get a single story detail
 router.get("/story/:storyid", async (request, response, next) => {
   try {
@@ -80,84 +77,128 @@ router.get("/story/:storyid", async (request, response, next) => {
       return response.status(403).send({ message: "no story found!" });
     }
     return response.status(200).send(story);
-
-
   } catch (error) {
     next(error);
   }
 });
 
 // Rate a story
-router.post(
-  "/story/:storyid/rate",
-  authMiddleware,
-  async (request, response, next) => {
-    try {
-      const { userId, ratingValue } = request.body;
-      const storyId = request.params.storyid;
-      const rating = await Rating.findOne({
-        where: {
+router.post("/story/:storyid/rate", auth, async (request, response, next) => {
+  try {
+    const { userId, ratingValue } = request.body;
+    const storyId = request.params.storyid;
+    const rating = await Rating.findOne({
+      where: {
+        userId: userId,
+        storyId: storyId,
+      },
+    });
+    const story = await Story.findOne({
+      where: {
+        id: storyId,
+      },
+      include: { model: User, attributes: ["name"] },
+    });
+    if (!story) {
+      res.status(400).send({ error: "Story does not exist" });
+    } else {
+      if (!rating) {
+        const newRating = await Rating.create({
           userId: userId,
           storyId: storyId,
-        },
-      });
-      const story = await Story.findOne({
-        where: {
-          id: storyId,
-        },
-        include: { model: User, attributes: ["name"] },
-      });
-      if (!story) {
-        res.status(400).send({ error: "Story does not exist" });
-      } else {
-        if (!rating) {
-          const newRating = await Rating.create({
-            userId: userId,
+          ratingValue: ratingValue,
+        });
+        const ratingsofAStory = await Rating.findAll({
+          where: {
             storyId: storyId,
-            ratingValue: ratingValue,
-          });
-          const ratingsofAStory = await Rating.findAll({
-            where: {
-              storyId: storyId,
-            },
-          });
-          const ratingValuesofAStory = ratingsofAStory.map(
-            (rating) => rating.ratingValue
-          );
+          },
+        });
+        const ratingValuesofAStory = ratingsofAStory.map(
+          (rating) => rating.ratingValue
+        );
 
-          const newRatingAverage = Math.round(
-            ratingValuesofAStory.reduce(function (a, b) {
-              return a + b;
-            }, 0) / ratingValuesofAStory.length
-          );
-          await story.update({ ratingAverage: newRatingAverage });
-          return response.status(200).send(story);
-        } else {
-          await rating.update({ ratingValue: ratingValue });
+        const newRatingAverage = Math.round(
+          ratingValuesofAStory.reduce(function (a, b) {
+            return a + b;
+          }, 0) / ratingValuesofAStory.length
+        );
+        await story.update({ ratingAverage: newRatingAverage });
+        return response.status(200).send(story);
+      } else {
+        await rating.update({ ratingValue: ratingValue });
 
-          const ratingsofAStory = await Rating.findAll({
-            where: {
-              storyId: storyId,
-            },
-          });
-          const ratingValuesofAStory = ratingsofAStory.map(
-            (rating) => rating.ratingValue
-          );
+        const ratingsofAStory = await Rating.findAll({
+          where: {
+            storyId: storyId,
+          },
+        });
+        const ratingValuesofAStory = ratingsofAStory.map(
+          (rating) => rating.ratingValue
+        );
 
-          const newRatingAverage = Math.round(
-            ratingValuesofAStory.reduce(function (a, b) {
-              return a + b;
-            }, 0) / ratingValuesofAStory.length
-          );
-          await story.update({ ratingAverage: newRatingAverage });
-          return response.status(200).send(story);
-        }
+        const newRatingAverage = Math.round(
+          ratingValuesofAStory.reduce(function (a, b) {
+            return a + b;
+          }, 0) / ratingValuesofAStory.length
+        );
+        await story.update({ ratingAverage: newRatingAverage });
+        return response.status(200).send(story);
       }
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//display all comments
+router.get("/story/:storyid/comments", async (request, response, next) => {
+  try {
+    const comments = await Comment.findAll({
+      where: { storyId: request.params.storyid },
+      include: [{ model: User, attributes: ["name", "imageUrl"] }],
+      order: [["createdAt", "DESC"]],
+    });
+    response
+      .status(200)
+      .send({ message: "All comments of this story", comments });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// add new comment
+
+router.post(
+  "/story/:storyid/comments",
+  auth,
+  async (request, response, next) => {
+    try {
+      const user = await User.findByPk(request.user.id);
+      const storyId = request.params.storyid;
+      const { content } = request.body; // reveive data from request
+      if (!content) {
+        return response
+          .status(400)
+          .send({ message: "A comment must have a content" });
+      }
+      const newComment = await Comment.create({
+        content,
+        userId: user.id,
+        storyId: storyId,
+      });
+
+      const comments = await Comment.findAll({
+        where: { storyId: storyId },
+        include: [{ model: User, attributes: ["name", "imageUrl"] }],
+        order: [["createdAt", "DESC"]],
+      });
+      response
+        .status(200)
+        .send({ message: "All comments of this story after adding", comments });
     } catch (error) {
       next(error);
     }
   }
 );
-
 
 module.exports = router;
